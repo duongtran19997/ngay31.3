c933eddbc0fdffd0c1ec2e0ae18b32b1e91fc06a
superman <duonga2qp@gmail.com>
2022-03-31 14:57:00 +0700

    nopbai

---
+++ Doyouloveme/doyouloveme.js
@@ -0,0 +1,9 @@
+function yes(){
+alert('love you too')
+}
+function no(){
+let x = Math.random()*window.innerWidth
+    let y = Math.random()*window.innerHeight
+    button.style.left = parseInt(button.style.left) + x + 'px'
+    button.style.top = parseInt(button.style.top) + y +'px'
+}
